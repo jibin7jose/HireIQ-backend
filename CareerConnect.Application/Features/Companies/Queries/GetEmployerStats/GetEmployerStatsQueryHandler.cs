e5942b4d79eb1175fede3cee54014cf12e4ee111
Jibin7Jose <jibinjose884@gmail.com>
2026-08-06 12:12:19 +0530

    fix: compile error with JobStatus

--- CareerConnect.Application/Features/Companies/Queries/GetEmployerStats/GetEmployerStatsQueryHandler.cs
+++ CareerConnect.Application/Features/Companies/Queries/GetEmployerStats/GetEmployerStatsQueryHandler.cs
@@ -1,13 +1,12 @@
 using CareerConnect.Application.DTOs.Companies;
 using CareerConnect.Application.Interfaces;
 using CareerConnect.Domain.Entities;
+using CareerConnect.Domain.Enums;
 using CareerConnect.Domain.Exceptions;
 using MediatR;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using CareerConnect.Infrastructure.Persistence; // Assuming this is needed for db context directly if repo isn't enough, but let's see.
 
 namespace CareerConnect.Application.Features.Companies.Queries.GetEmployerStats;
 
@@ -27,7 +26,7 @@ public sealed class GetEmployerStatsQueryHandler : IRequestHandler<GetEmployerSt
         var company = await _companyRepository.GetByAdminUserIdAsync(request.AdminUserId, cancellationToken)
             ?? throw new NotFoundException(nameof(Company), request.AdminUserId);
 
-        var activeJobs = company.Jobs.Count(j => j.IsActive);
+        var activeJobs = company.Jobs.Count(j => j.Status == JobStatus.Open);
         
         int totalApplicants = await _applicationRepository.GetTotalApplicantsByCompanyIdAsync(company.Id, cancellationToken);
 
